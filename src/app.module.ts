612366fd740faa8a7282bacd00186e99dfab8e68
tishakong <leehn45@naver.com>
2024-04-30 22:07:33 +0900

    Init

--- src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-
-@Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI ?? 'mongodb://localhost:27017/mydatabase')],
-})
-
-export class AppModule {}
