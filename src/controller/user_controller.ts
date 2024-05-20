06a2cab82e1ab78b8fdd6b6907eb632888a486b7
eonwy <yeawon6566@gmail.com>
2024-05-20 19:04:55 +0900

    FIX:controller fix

--- src/controller/user_controller.ts
+++ src/controller/user_controller.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import { Result, ValidationError, validationResult } from "express-validator";
 import * as user_service from "../service/user/user_service";
+import { user_info } from "../dto/user/user_info";
 
 const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void | Response> => {
+
+  const user_info: user_info = req.body
+
   try {
-    const data = await user_service.create_user;
+    const data = await user_service.create_user(user_info);
     console.log(data);
 
     return res.send("create_user success!");
