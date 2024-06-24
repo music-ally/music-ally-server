9c07e534940ac4eb39ab38e1861178c9a62ed128
solux <leehn45@naver.com>
2024-06-25 00:37:21 +0900

    MOD: user_update DTO 수정

--- src/dto/user/request/user_update.ts
+++ src/dto/user/request/user_update.ts
@@ -1,7 +1,7 @@
 export interface user_update_dto {
-  password: string;
-  nickname: string;
-  birthday: string;
-  sex: string;
-  profile_image: string;
+  password?: string;
+  nickname?: string;
+  birthday?: string;
+  homearea_name?: string;
+  profile_image?: string;
 }
