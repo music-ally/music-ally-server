428033ce7347b33bf5b5140b756e7c51f0023433
solux <leehn45@naver.com>
2024-06-24 17:33:27 +0900

    FEAT: Actor DTO 생성

---
+++ src/dto/actor/response/actor_detail_res.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import { review_res_dto } from "../../review/response/review_res";
+import { musical_main_item_dto } from "../../musical/response/musical_main_res";
+
+export interface actor_detail_res_dto{
+    actor_id: mongoose.Types.ObjectId;
+    profile_image: string;
+    actor_name: string;
+    birthday?: Date; 
+    debut?: Date;
+    agency?: string;
+    job?: string;
+    physical?: string;
+    works_count: Number; //works 리스트 내 개수로 파악할 수 있긴 한데 별도로 줄지?
+    works: musical_main_item_dto[];
+    reviews:review_res_dto[];
+}
