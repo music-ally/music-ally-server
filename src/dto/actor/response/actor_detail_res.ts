bef4110a4b00569cba8b79e74b2256c52560c947
solux <leehn45@naver.com>
2024-06-25 00:38:50 +0900

    MOD: DTO 내 Number -> number로 수정 3

--- src/dto/actor/response/actor_detail_res.ts
+++ src/dto/actor/response/actor_detail_res.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-import { review_main_item_dto } from "../../review/response/review_res";
+import { review_main_item_dto } from "../../review/response/review_main_res";
 import { musical_main_item_dto } from "../../musical/response/musical_main_res";
 
 export interface actor_detail_res_dto{
@@ -11,7 +11,7 @@ export interface actor_detail_res_dto{
     agency?: string;
     job?: string;
     physical?: string;
-    works_count: Number; //works 리스트 내 개수로 파악할 수 있긴 한데 별도로 줄지?
+    works_count: number; //works 리스트 내 개수로 파악할 수 있긴 한데 별도로 줄지?
     works: musical_main_item_dto[];
     reviews:review_main_item_dto[];
 }
