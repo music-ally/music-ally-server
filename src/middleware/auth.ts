6a0c06b68e37c22232ed691b7ce6e02346b2ea34
tishakong <leehn45@naver.com>
2024-06-25 19:25:43 +0900

    MOD: JWT 코드 인증 관련 res 수정

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,11 +1,19 @@
+import form from "../utils/response_form";
+import message from "../utils/response_message";
+import status_code from "../utils/status_code";
 import { Request, Response, NextFunction } from 'express';
 import { verifyToken } from "../utils/jwt_handler";
+import { error } from "console";
 
 const auth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
   
   if (!authHeader) {
-    return res.status(401).json({ message: 'No token provided' });
+    return res
+      .status(status_code.UNAUTHORIZED)
+      .send(
+        form.fail(message.NULL_VALUE_TOKEN)
+      );
   }
 
   const token = authHeader.split(' ')[1];
@@ -15,7 +23,11 @@ const auth = (req: Request, res: Response, next: NextFunction) => {
     req.token = user;
     next();
   } catch (err) {
-    return res.status(401).json({ message: 'Invalid token' });
+    return res
+      .status(status_code.UNAUTHORIZED)
+      .send(
+        form.fail(message.INVALID_TOKEN)
+      );
   }
 };
 
