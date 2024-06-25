bbbd86e894179799d0c7f76199a5a8757cd3bb7a
solux <leehn45@naver.com>
2024-06-25 14:02:55 +0900

    FEAT: notification DTO 생성 및 수정

---
+++ src/dto/notification/response/notification_res.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+export interface notification_res_dto {
+  notifications: notification_item_dto[];
+}
+
+export interface notification_item_dto {
+  type: string;
+  create_at: Date;
+  review_id?: mongoose.Types.ObjectId;
+  poster_image?: string;
+  review_like_nickname?: string;
+  review_like_image?: string[];
+  review_like_num?: number;
+  follower_id?: mongoose.Types.ObjectId;
+  follower_image?: string;
+  follower_nickname?: string;
+  is_followed?: boolean; //내가 팔로우 하고 있는지 여부
+}
