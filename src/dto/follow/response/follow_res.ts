c8065e9063914c2d80672700d2c043addad0fff7
YeaWon Kim <yeawon6566@gmail.com>
2024-06-30 04:51:05 +0900

    FEAT : 마이페이지에 필요한 API 서비스 모두 생성

--- src/dto/follow/response/follow_res.ts
+++ src/dto/follow/response/follow_res.ts
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 export interface follow_res_dto {
-    followings: follow_item_dto[];
+    follow_list: follow_item_dto[];
 }
 
 export interface follow_item_dto {
     user_id: mongoose.Types.ObjectId;
     nickname: string;
     email: string; 
-    is_following: boolean; // 내가 상대방을 팔로우하고 있는지 여부
-    is_follower: boolean; // 상대방이 나를 팔로우하고 있는지 여부
+    both_follow: boolean; // 맞팔 여부 T:맞팔o F:맞팔x
 }
 
