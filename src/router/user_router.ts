ad074876e6c9a3b88a062e9fe01d00308a2eabf0
tishakong <leehn45@naver.com>
2024-07-16 17:04:21 +0900

    FEAT: 회원 탈퇴 기능 구현
    회원 탈퇴 후 로그인 시도시 회원 탈퇴 유저라는 에러 발생

--- src/router/user_router.ts
+++ src/router/user_router.ts
@@ -5,7 +5,7 @@ import auth from "../middleware/auth";
 const user_router = express.Router();
 
 user_router.post("/auth/join", user_controller.join_user);
-user_router.patch("/auth/leave", auth, user_controller.deleteUser);
+user_router.patch("/auth/leave", auth, user_controller.leave);
 user_router.get("/auth/login", user_controller.login);
 user_router.get("/auth/logout", auth, user_controller.logout);
 user_router.get("/auth/check/email", user_controller.check_email);
