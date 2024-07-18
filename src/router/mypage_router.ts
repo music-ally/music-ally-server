a06f4dcb804c7c9aeb1268454947ca614399974d
eonwy <yeawon6566@gmail.com>
2024-07-18 22:30:57 +0900

    FIX : 오타 수정

--- src/router/mypage_router.ts
+++ src/router/mypage_router.ts
@@ -2,19 +2,19 @@ import express from "express";
 import * as mypage_controller from "../controller/mypage_controller";
 import auth from "../middleware/auth";
 
-const mypge_router = express.Router();
+const mypage_router = express.Router();
 
-// mypge_router.get("/myPage", mypage_controller.fetch_my_profile);
-mypge_router.get("/myPage/:userId", mypage_controller.fetch_my_profile);
-mypge_router.get("/myPage/review/:reviewId", mypage_controller.fetch_mypage_review_detail);
-// mypge_router.get("/myPage/follower", mypage_controller.fetch_follower);
-// mypge_router.get("/myPage/following", mypage_controller.fetch_following);
-mypge_router.get("/myPage/follower/:userId", mypage_controller.fetch_follower);
-mypge_router.get("/myPage/following/:userId", mypage_controller.fetch_following);
+// mypage_router.get("/myPage", mypage_controller.fetch_my_profile);
+mypage_router.get("/myPage/:userId", mypage_controller.fetch_my_profile);
+mypage_router.get("/myPage/review/:reviewId", mypage_controller.fetch_mypage_review_detail);
+// mypage_router.get("/myPage/follower", mypage_controller.fetch_follower);
+// mypage_router.get("/myPage/following", mypage_controller.fetch_following);
+mypage_router.get("/myPage/follower/:userId", mypage_controller.fetch_follower);
+mypage_router.get("/myPage/following/:userId", mypage_controller.fetch_following);
 
-// mypge_router.patch("/myPage", mypage_controller.update_profile);
-mypge_router.patch("/myPage/:userId", mypage_controller.update_profile);
+// mypage_router.patch("/myPage", mypage_controller.update_profile);
+mypage_router.patch("/myPage/:userId", mypage_controller.update_profile);
 
-mypge_router.delete("/myPage/review/:reviewId", mypage_controller.delete_review);
+mypage_router.delete("/myPage/review/:reviewId", mypage_controller.delete_review);
 
-export default mypge_router;
+export default mypage_router;
