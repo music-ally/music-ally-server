bf7cd6edcb301993804352cf306d9a0837edcf71
eonwy <yeawon6566@gmail.com>
2024-07-24 23:05:11 +0900

    FIX : 메서드 수정

--- src/router/notification_router.ts
+++ src/router/notification_router.ts
@@ -5,6 +5,6 @@ import auth from "../middleware/auth";
 const notification_router = express.Router();
 
 notification_router.get("/notification", auth, notification_controller.fetch_notifications);
-notification_router.get("/notification/onoff", auth, notification_controller.change_notification);
+notification_router.patch("/notification/onoff", auth, notification_controller.change_notification);
 
 export default notification_router;
