08fe08d1004eacfd4910a6de94192df59b5fc1d7
eonwy <yeawon6566@gmail.com>
2024-07-27 11:41:42 +0900

    FIX : 라우터 수정

--- src/router/crawler_router.ts
+++ src/router/crawler_router.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import * as crawler_controller from '../controller/crawler_controller';
 
 const crawler_router = Router();
-// get_artists_controller = 
+
 crawler_router.get('/crawling/musical', crawler_controller.get_musicals_controller);
 crawler_router.get('/crawling/actor', crawler_controller.get_actors_controller);
 crawler_router.get('/crawling/theater', crawler_controller.get_theaters_controller);
-// crawler_router.post('/crawling', crawler_controller.get_musicals_controller);
+crawler_router.post('/crawling/save/musical', crawler_controller.save_musicals_controller);
+crawler_router.post('/crawling/save/actor', crawler_controller.save_actors_controller);
+crawler_router.post('/crawling/save/theater', crawler_controller.save_theaters_controller);
 
 export default crawler_router;
