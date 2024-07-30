cbe60964d5c335279841d97c5090b0799dcd6193
tishakong <leehn45@naver.com>
2024-07-30 16:38:50 +0900

    FEAT: 소셜 로그인 구현

---
+++ src/dto/user/request/user_social_login.ts
@@ -0,0 +1,4 @@
+export interface user_social_login_dto {
+  email: string;
+  social_id: string;
+}
--- src/service/user/user_service.ts
+++ src/service/user/user_service.ts
@@ -7,6 +7,7 @@ import Areas from "../../schema/areas";
 import bcrypt from 'bcryptjs';
 import { generate_access_token, generate_refresh_token } from "../../utils/jwt_handler";
 import Blacklists from "../../schema/jwt_blacklist";
+import { user_social_login_dto } from "../../dto/user/request/user_social_login";
 
 
 
@@ -25,11 +26,16 @@ const find_homearea_by_name = async(homearea : string) => {
 const join_user = async (user_join_dto : user_join_dto) => {
   try {
     let hashed_password = null
+    let hashed_social_id = null
     const user_homearea_id = await find_homearea_by_name(user_join_dto.homearea_name)
     const birthday = new Date(user_join_dto.birthday)
     const sex = user_join_dto.sex === "여성" ? true : false; //true === 여성, false === 남성
     if (user_join_dto.password){
       hashed_password = await bcrypt.hash(user_join_dto.password, 10);
+    }
+    
+    if (user_join_dto.social_id){
+      hashed_social_id = await bcrypt.hash(user_join_dto.social_id, 10);
     }
     
     const user = new Users({
@@ -41,7 +47,7 @@ const join_user = async (user_join_dto : user_join_dto) => {
       homearea: user_homearea_id,
       noti_allow: true,
       signup_method: user_join_dto.signup_method,
-      social_id: user_join_dto.social_id
+      social_id: hashed_social_id
     });
 
     await user.save();
@@ -103,6 +109,31 @@ const login_user = async (user_login_dto : user_login_dto): Promise<user_login_r
 };
 
 
+const social_login = async (user_login_dto : user_social_login_dto): Promise<user_login_res_dto> => {
+  const user = await Users.findOne({email : user_login_dto.email});
+  
+  if (!user){
+    throw new Error('email not found');
+  } else if (!user.social_id){
+    throw new Error('wrong login method');
+  }
+    else if (!(await bcrypt.compare(user_login_dto.social_id, user.social_id))) {
+    throw new Error('wrong social_id');
+  } else if (!user.is_active){
+    throw new Error('left user');
+  }
+  
+  const access_token = generate_access_token(user._id);
+  const refresh_token = generate_refresh_token(user._id);
+
+  return { 
+    email: user.email,
+    access_token, 
+    refresh_token 
+  };
+};
+
+
 const check_email = async (email : string) => {
   const user = await Users.findOne({email : email});
   let is_duplicate;
@@ -112,12 +143,10 @@ const check_email = async (email : string) => {
     is_duplicate = true
   }
 
-
   return {
     is_duplicate : is_duplicate,
     signup_method : user?.signup_method
   }
-
 }  
 
 const check_nickname = async (nickname : string): Promise<boolean> => {
@@ -145,6 +174,7 @@ export {
   leave,
   find_homearea_by_name,
   login_user,
+  social_login,
   check_email,
   check_nickname,
   logout
