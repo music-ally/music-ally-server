e48c75771eccd84450d35845f12323d81f6ae65a
tishakong <leehn45@naver.com>
2024-08-03 17:42:20 +0900

    FIX: router 순서 조정

--- src/router/mypage_router.ts
+++ src/router/mypage_router.ts
@@ -9,9 +9,10 @@ mypage_router.get("/myPage", auth, mypage_controller.fetch_my_profile);
 mypage_router.get("/myPage/review/:reviewId", auth, mypage_controller.fetch_mypage_review_detail);
 mypage_router.get("/myPage/follower", auth, mypage_controller.fetch_follower);
 mypage_router.get("/myPage/following", auth, mypage_controller.fetch_following);
-mypage_router.patch("/myPage", auth, upload.single('profile_image'), processFile, mypage_controller.update_profile);
 mypage_router.patch("/myPage/profile/image", auth, upload.single('profile_image'), processFile, mypage_controller.update_profile_image);
 mypage_router.patch("/myPage/profile/text", auth, mypage_controller.update_profile_text);
+mypage_router.patch("/myPage", auth, upload.single('profile_image'), processFile, mypage_controller.update_profile);
+
 
 mypage_router.delete("/myPage/review/:reviewId", auth, mypage_controller.delete_review);
 
